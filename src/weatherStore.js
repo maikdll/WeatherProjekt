0118cf9c88486433a07e4e7f5d2fa160bfcd2073
maik.made <maikmadej99@gmail.com>
2025-01-15 23:58:42 +0100

    sec commit

--- src/weatherStore.js
+++ src/weatherStore.js
@@ -19,19 +19,25 @@ export const fetchWeatherData = async (city) => {
         date: "2024-12-12",
         temperature: { current: 10, min: 20, max: 30 },
         humidity: 70,
-        weather: "sunny",
+        weather: "lightRain",
       },
       {
         date: "2024-12-13",
         temperature: { current: 20, min: 10, max: 30 },
         humidity: 40,
-        weather: "cloudy",
+        weather: "drizzle",
       },
       {
         date: "2024-12-14",
         temperature: { current: 10, min: 0, max: 40 },
         humidity: 20,
-        weather: "rainy",
+        weather: "clearSky",
+      },
+      {
+        date: "2024-12-15",
+        temperature: { current: 15, min: 2, max: 20 },
+        humidity: 20,
+        weather: "fog",
       },
     ],
   };
